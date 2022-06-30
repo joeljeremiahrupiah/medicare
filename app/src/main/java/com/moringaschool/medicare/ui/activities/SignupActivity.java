4d4e765652c2a3683742ce660d7fc7a5002ed1f3
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-30 10:35:30 +0300

    Project setup

---
+++ app/src/main/java/com/moringaschool/medicare/ui/activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.medicare.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.medicare.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
