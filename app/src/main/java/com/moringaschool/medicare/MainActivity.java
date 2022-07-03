81e4f8b6fed8eee619e2e360b6326053baa9dc58
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-07-03 16:00:23 +0300

    Add navigation drawer

--- app/src/main/java/com/moringaschool/medicare/MainActivity.java
+++ app/src/main/java/com/moringaschool/medicare/MainActivity.java
@@ -2,13 +2,26 @@ package com.moringaschool.medicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.moringaschool.medicare.ui.activities.DoctorsActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = (TextView) findViewById(R.id.hello);
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DoctorsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/moringaschool/medicare/ui/activities/DoctorsActivity.java
+++ app/src/main/java/com/moringaschool/medicare/ui/activities/DoctorsActivity.java
@@ -1,9 +1,16 @@
 package com.moringaschool.medicare.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
 import com.moringaschool.medicare.R;
 
 public class DoctorsActivity extends AppCompatActivity {
@@ -12,5 +19,25 @@ public class DoctorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctors);
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menuOrders:
+                        Intent intent = new Intent(DoctorsActivity.this, MedicineActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
