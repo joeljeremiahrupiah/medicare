f46e0d72b5e18609839a40b5acf59bc71cac9113
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-07-05 14:45:39 +0300

    update

--- app/src/main/java/com/moringaschool/medicare/MainActivity.java
+++ app/src/main/java/com/moringaschool/medicare/MainActivity.java
@@ -11,30 +11,10 @@ import com.moringaschool.medicare.ui.activities.LoginActivity;
 import com.moringaschool.medicare.ui.activities.SignupActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView login;
-    private TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        login = findViewById(R.id.tvLogin);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        signup = findViewById(R.id.tvSignup);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
